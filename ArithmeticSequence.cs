a76b90d2d3b9ac89770a0802b257adfc4d1224de
Максим <sdalll@mail.ru>
2019-02-17 00:57:09 +0300

    Task2

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,8 +6,20 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (count < 0)
+            {
+                throw new ArgumentException();
+            }
+                int sum = 0;
+                // put your code here
+                //throw new NotImplementedException();
+                for (int x = 0; x < count; x++)
+                {
+                    sum = sum + number + add * x;
+
+                }
+                return sum;
+            
         }
     }
 }
